1e0b69166328c06c0fb54606de077ee63a46d78a
PedroValerio7 <88409097+PedroValerio7@users.noreply.github.com>
2021-08-30 17:51:45 -0500

    commit

---
+++ forLoopB.py
@@ -0,0 +1,34 @@
+"""
+for i in range(151):
+    print(i)
+"""
+"""
+for a in range(5,1005,5):
+    print(a)
+"""
+"""
+for b in range(1,101):
+    if b%10==0:
+        print("Coding Dojo")
+    elif b%5==0:
+        print("Coding")
+    else:
+        print(b)
+"""
+"""
+sum=0
+for c in range(1,500000,2):
+    sum += c
+print(sum)
+"""
+"""
+for d in range(2018,0,-4):
+    print(d)
+"""
+
+lowNum = 2
+highNum = 9
+mult = 3
+
+for e in range(lowNum+1,highNum +1,mult):
+    print(e)
